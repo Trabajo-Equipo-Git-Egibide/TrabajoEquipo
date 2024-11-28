f759269c87bd905b5ebdb47e125767ec67db4241
Laura <laura.gomez@ikasle.egibide.org>
2024-11-28 20:59:10 +0100

    Inicializando proyecyo

---
+++ Trabajo_Equipo_Git/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void registrarTareas() {
+
+    }
+
+    public void visualizarTareas() {
+
+    }
+
+    public void actualizarTareas() {
+
+    }
+
+    public void eliminarTareas() {
+        
+    }
+}
